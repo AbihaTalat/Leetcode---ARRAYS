e2f5d3e7fbdfd63f97859dd63cc4e16f2e2d71ac
AbihaTalat <works.abihat461@gmail.com>
2026-06-18 03:53:16 +0500

    Solutio

---
+++ 1732. Find the Highest Altitude/Solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+
+       int max = 0;
+       int current = 0;
+       for (int i = 0; i < gain.size(); i++)
+       {
+          current += gain[i];
+          if (current > max)
+          {
+            max = current;
+          }
+
+       }
+        return max;
+    }
+};
